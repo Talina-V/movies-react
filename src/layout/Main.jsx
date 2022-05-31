d923ddcef683b36654a60ca4cec9877ab092fdc9
Talina <talina@MacBook-Pro-Talina.local>
2022-05-31 14:31:15 +0300

    final adjustments

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,6 +4,8 @@ import { MoviesList } from '../components/MoviesList';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends Component {
 
     state = {
@@ -12,14 +14,14 @@ class Main extends Component {
     }
 
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=f2c9ab06&s=matrix')
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
     }
 
     searchMovies = (str, type = 'all') => {
         this.setState({loading: true});
-        fetch(`http://www.omdbapi.com/?apikey=f2c9ab06&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false}))
     }
